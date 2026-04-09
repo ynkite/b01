9dd6d1005de0ac059a72d92a18ae0af93972df05
ynkite <tkddus3521@naver.com>
2026-04-09 21:13:15 +0900

    26-04-09 1. 유효성 검사 (Validation) 설정 2. 게시글 등록 기능 (Register - Create) 3. 게시글 상세 조회 기능 (Read - Read) 4. 게시글 수정 기능 (Modify - Update) 5. 게시글 삭제 기능 (Remove - Delete)

--- src/main/java/com/example/b01/controller/BoardController.java
+++ src/main/java/com/example/b01/controller/BoardController.java
@@ -5,12 +5,16 @@ import com.example.b01.dto.BoardDTO;
 import com.example.b01.dto.PageRequestDTO;
 import com.example.b01.dto.PageResponseDTO;
 import com.example.b01.service.BoardService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/board")
@@ -25,5 +29,60 @@ public class BoardController {
         PageResponseDTO<BoardDTO> responseDTO = boardService.list(pageRequestDTO);
         log.info(responseDTO);
         model.addAttribute("responseDTO", responseDTO);
+    }
+
+    @GetMapping("/register")
+    public void registerGET() {
+
+    }
+
+    @PostMapping("/register")
+    public String registerPost(@Valid BoardDTO boardDTO, BindingResult bindingResult,
+                             RedirectAttributes redirectAttributes) {
+        log.info("board POST register.........");
+        if(bindingResult.hasErrors()) {
+            redirectAttributes.addFlashAttribute("errors",
+                    bindingResult.getAllErrors());
+            return "redirect:/board/register";
+        }
+        log.info(boardDTO);
+        Long bno = boardService.register(boardDTO);
+        redirectAttributes.addFlashAttribute("result", bno);
+        return "redirect:/board/list";
+    }
+
+//    @GetMapping("/read")
+    @GetMapping({"/read", "/modify"})
+    public void read(Long bno, PageRequestDTO pageRequestDTO, Model model) {
+        BoardDTO boardDTO = boardService.readOne(bno);
+        log.info(boardDTO);
+        model.addAttribute("dto", boardDTO);
+    }
+
+    @PostMapping("/modify")
+    public String modify(@Valid BoardDTO boardDTO,
+                         BindingResult bindingResult,
+                         PageRequestDTO pageRequestDTO,
+                         RedirectAttributes redirectAttributes){
+        log.info("board modify post.........." + boardDTO);
+        if(bindingResult.hasErrors()) {
+            log.info("has errors..........");
+            String link = pageRequestDTO. getLink();
+            redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
+            redirectAttributes.addAttribute("bno", boardDTO.getBno());
+            return "redirect:/board/modify?"+link;
+        }
+        boardService.modify(boardDTO);
+        redirectAttributes.addFlashAttribute("result", "modified");
+        redirectAttributes.addAttribute("bno", boardDTO.getBno());
+        return "redirect:/board/read";
+    }
+
+    @GetMapping("remove")
+    public String remove(Long bno, RedirectAttributes redirectAttributes) {
+        log.info("remove post...." + bno);
+        boardService.remove(bno);
+        redirectAttributes.addFlashAttribute("result", "removed");
+        return "redirect:/board/list";
     }
 }
--- src/main/java/com/example/b01/dto/BoardDTO.java
+++ src/main/java/com/example/b01/dto/BoardDTO.java
@@ -1,5 +1,7 @@
 package com.example.b01.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,7 +15,10 @@ import java.time.LocalDateTime;
 
 public class BoardDTO {
     private Long bno;
+    @NotEmpty
+    @Size(min = 3, max = 100)
     private String title;
+    @NotEmpty
     private String content;
     private String writer;
     private LocalDateTime regDate;
